adba629720cbffbce0e69cb5b740b5b9a0abee56
russodelsur <ruslan@m2measure.co.uk>
2023-04-11 22:06:01 +0100

    privacy update

--- src/components/pages/Privacy.js
+++ src/components/pages/Privacy.js
@@ -3,68 +3,68 @@ import React from 'react';
 function Privacy() {
         return (
             <div className='privacy'>
-                Martin Brudnizki Design Studio is committed to protecting and respecting your privacy.
+        <p>Made4living Design Studio is committed to protecting and respecting your privacy.</p>
 
-This privacy notice together with our terms of use sets out the basis on which any personal data we collect from you, or that you provide to us, will be processed by us.
+        <p>This privacy notice together with our terms of use sets out the basis on which any personal data we collect from you, or that you provide to us, will be processed by us.</p>
 
-For the purpose of the General Data Protection Regulations (GDPR) the data controller is Martin Brudnizki Design Studio, UNIT 1G, Chelsea Reach, 79-89 Lots Road, London, SW10 0RN.
+        <p> For the purpose of the General Data Protection Regulations (GDPR) the data controller is Made4living Design Studio, 1 Hull close, SE16 6BY.</p>
 
-Information we may collect from you
-We may collect and process the following data about you:
+        <p>Information we may collect from you</p>
+        <p> We may collect and process the following data about you:</p>
 
-Information that you provide by filling in forms on our website site www.mbds.com This includes information provided at the time of registering to use our site, subscribing to our service, posting material or requesting further services. We may also ask you for information when you report a problem with our site 
-If you contact us, we may keep a record of that correspondence
-We may also ask you to complete surveys that we use for research purposes, although you do not have to respond to them
-Details of your visits to our site including, but not limited to, traffic data, location data, weblogs and other communication data, and the resources that you access.
-IP addresses and cookies
-We may collect information about your computer, including where available your IP address, operating system and browser type, for system administration. This is statistical data about our users' browsing actions and patterns, and does not identify any individual.
-For the same reason, we may obtain information about your general internet usage by using a cookie file which is stored on the hard drive of your computer. Cookies contain information that is transferred to your computer's hard drive. They help us to improve our site and to deliver a better and more personalised service.
-They enable us:
-To estimate our audience size and usage pattern. 
-To store information about your preferences, and so allow us to customise our site according to your individual interests. 
-To speed up your searches. 
-To recognise you when you return to our site
-Opting out of cookies
-You may refuse to accept cookies by activating the setting on your browser which allows you to refuse the setting of cookies. However, if you select this setting you may be unable to access certain parts of our site. Unless you have adjusted your browser setting so that it will refuse cookies, our system will issue cookies when you log on to our site.
+        <p> Information that you provide by filling in forms on our website site www.mbds.com This includes information provided at the time of registering to use our site, subscribing to our service, posting material or requesting further services. We may also ask you for information when you report a problem with our site </p>
+        <p> If you contact us, we may keep a record of that correspondence</p>
+        <p> We may also ask you to complete surveys that we use for research purposes, although you do not have to respond to them</p>
+        <p> Details of your visits to our site including, but not limited to, traffic data, location data, weblogs and other communication data, and the resources that you access.</p>
+        <p> IP addresses and cookies:</p>
+        <p> We may collect information about your computer, including where available your IP address, operating system and browser type, for system administration. This is statistical data about our users' browsing actions and patterns, and does not identify any individual.</p>
+        <p> For the same reason, we may obtain information about your general internet usage by using a cookie file which is stored on the hard drive of your computer. Cookies contain information that is transferred to your computer's hard drive. They help us to improve our site and to deliver a better and more personalised service.</p>
+        <p> They enable us:</p>
+        <p> To estimate our audience size and usage pattern. </p>
+        <p> To store information about your preferences, and so allow us to customise our site according to your individual interests. </p>
+        <p> To speed up your searches. </p>
+        <p> To recognise you when you return to our site</p>
+        <p> Opting out of cookies</p>
+        <p> You may refuse to accept cookies by activating the setting on your browser which allows you to refuse the setting of cookies. However, if you select this setting you may be unable to access certain parts of our site. Unless you have adjusted your browser setting so that it will refuse cookies, our system will issue cookies when you log on to our site.</p>
 
-Where we store your personal data
-The data that we collect from you may be transferred to, and stored at, a destination outside the European Economic Area (EEA). It may also be processed by staff operating outside the EEA who work for us or for one of our suppliers. Such staff maybe engaged in, among other things, the fulfilment of your order, the processing of your payment details and the provision of support services. By submitting your personal data, you agree to this transfer, storing or processing. We will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this privacy policy.
+        <p> Where we store your personal data</p>
+        <p> The data that we collect from you may be transferred to, and stored at, a destination outside the European Economic Area (EEA). It may also be processed by staff operating outside the EEA who work for us or for one of our suppliers. Such staff maybe engaged in, among other things, the fulfilment of your order, the processing of your payment details and the provision of support services. By submitting your personal data, you agree to this transfer, storing or processing. We will take all steps reasonably necessary to ensure that your data is treated securely and in accordance with this privacy policy.</p>
 
-All information you provide to us is stored on our secure servers. However, the transmission of information via the internet is not completely secure.
+        <p> All information you provide to us is stored on our secure servers. However, the transmission of information via the internet is not completely secure.</p>
 
-Although we will do our best to protect your personal data, we cannot guarantee the security of your data transmitted to our site; any transmission is at your own risk. Once we have received your information, we will use strict procedures and security features to try to prevent unauthorised access.
+        <p> Although we will do our best to protect your personal data, we cannot guarantee the security of your data transmitted to our site; any transmission is at your own risk. Once we have received your information, we will use strict procedures and security features to try to prevent unauthorised access.</p>
 
-Uses made of the information
-We use information held about you in the following ways:
-To ensure that content from our site is presented in the most effective manner for you and for your computer
-To provide you with information, products or services that you request from us or which we feel may interest you, where you have consented to be contacted for such purposes. 
-To carry out our obligations arising from any contracts entered into between you and us
-To allow you to participate in interactive features of our service, when you choose to do so
-To notify you about changes to our service
-We may also use your data, or permit selected third parties to use your data, to provide you with information about goods and services which may be of interest to you and we or they may contact you about these by post or telephone.
+        <p> Uses made of the information</p>
+        <p> We use information held about you in the following ways:</p>
+        <p> To ensure that content from our site is presented in the most effective manner for you and for your computer</p>
+        <p> To provide you with information, products or services that you request from us or which we feel may interest you, where you have consented to be contacted for such purposes. </p>
+        <p> To carry out our obligations arising from any contracts entered into between you and us</p>
+        <p> To allow you to participate in interactive features of our service, when you choose to do so</p>
+        <p> To notify you about changes to our service</p>
+        <p> We may also use your data, or permit selected third parties to use your data, to provide you with information about goods and services which may be of interest to you and we or they may contact you about these by post or telephone.</p>
 
-If you are an existing customer, we will only contact you by electronic means (e-mail or SMS) with information about goods and services similar to those which were the subject of a previous sale to you.
+       <p>  If you are an existing customer, we will only contact you by electronic means (e-mail or SMS) with information about goods and services similar to those which were the subject of a previous sale to you.</p>
 
-If you are a new customer, and where we permit selected third parties to use your data, we (or they) will contact you by electronic means only if you have consented to this.
+        <p> If you are a new customer, and where we permit selected third parties to use your data, we (or they) will contact you by electronic means only if you have consented to this.</p>
 
-Disclosure of your information
-We may disclose your personal information to third parties:
-If we are under a duty to disclose or share your personal data in order to comply with any legal obligation, or in order to enforce or apply our terms of use and other agreements; or to protect the rights, property, or safety of Martin Brudnizki Design Studio and or our customers, or others. 
-In the event that we sell or buy any business or assets, in which case we may disclose your personal data to the prospective seller or buyer of such business or assets.
-This includes exchanging information with other companies and organisations for the purposes of fraud protection and credit risk reduction.
+        <p> Disclosure of your information</p>
+        <p> We may disclose your personal information to third parties:</p>
+        <p> If we are under a duty to disclose or share your personal data in order to comply with any legal obligation, or in order to enforce or apply our terms of use and other agreements; or to protect the rights, property, or safety of Made4living Design Studio and or our customers, or others. </p>
+       <p> In the event that we sell or buy any business or assets, in which case we may disclose your personal data to the prospective seller or buyer of such business or assets.</p>
+        <p> This includes exchanging information with other companies and organisations for the purposes of fraud protection and credit risk reduction.</p>
 
-Your rights
-You have the right to ask us not to process your personal data for marketing purposes. We will usually inform you (before collecting your data) if we intend to use your data for such purposes or if we intend to disclose your information to any third party for such purposes. You can exercise your right to prevent such processing by checking certain boxes on the forms we use to collect your data. You can also exercise the right at any time by contacting studio@mbds.com.
+       <p>  Your rights</p>
+        <p> You have the right to ask us not to process your personal data for marketing purposes. We will usually inform you (before collecting your data) if we intend to use your data for such purposes or if we intend to disclose your information to any third party for such purposes. You can exercise your right to prevent such processing by checking certain boxes on the forms we use to collect your data. You can also exercise the right at any time by contacting studio@mbds.com.</p>
 
-Access to information
-The Act gives you the right to access information held about you. Your right of access can be exercised in accordance with the Act.
+        <p> Access to information</p>
+        <p> The Act gives you the right to access information held about you. Your right of access can be exercised in accordance with the Act.</p>
 
-Changes to our privacy policy
-Any changes we may make to our privacy policy in the future will be posted on this page and, where appropriate, notified to you by e-mail.
+       <p>  Changes to our privacy policy</p>
+        <p> Any changes we may make to our privacy policy in the future will be posted on this page and, where appropriate, notified to you by e-mail.</p>
 
-Contact
-Questions, comments and requests regarding this privacy policy are welcomed and should be emailed to studio@mbds.com.
-            </div>
+        <p> Contact</p>
+        <p> Questions, comments and requests regarding this privacy policy are welcomed and should be emailed to info@made4living.co.uk.</p>
+        </div>
         );
     }
 
