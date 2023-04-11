ad5afd577b8baae18fe3cbb97de8a2e1e42056dc
russodelsur <ruslan@m2measure.co.uk>
2023-04-11 21:55:24 +0100

    never used fixed

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
-import logo from './logo.svg';
 import './App.css';
-import {React, useState, useEffect} from 'react';
+import {React} from 'react';
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Footer from './components/Footer';
 import Work from './components/pages/Work';
