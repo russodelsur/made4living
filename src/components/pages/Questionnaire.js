0034e304d83591320a4a242a1b53ba051b6c060a
russodelsur <ruslan@m2measure.co.uk>
2023-05-15 19:00:05 +0100

    updates to questionnare

--- src/components/pages/Questionnaire.js
+++ src/components/pages/Questionnaire.js
@@ -1,10 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { FormCheck, FormControl, Form, Button } from 'react-bootstrap';
 
 function Questionnaire() {
-	const questions = [
+	let questions = [
 		{
 			questionText: 'What are you looking for? You can select more than one option.',
+			type: "radio",
 			answerOptions: [
 				{ answerText: 'I’m looking to find a property to buy or I have already one in mind but I would like a professional opinion on it.(Currently available only in the London area)', index: "property" },
 				{ answerText: "I’m looking for architectural, interior design or simple furnishing purchase", index: "design"},
@@ -19,6 +20,10 @@ function Questionnaire() {
 		{
 			questionText: 'Tell us more regarding the property you are looking to buy. Where would you like to buy your dream property? Please list all desired locations.',
 			type: "input",
+			answerOptions: [
+				{ answerText: 'Ready to move in'},
+				{ answerText: "Renovation"},
+			],
 			},
 		{
 			questionText: 'Are you looking to purchase a property ready to move-in or also in need of renovation? You can select both.',
@@ -64,16 +69,73 @@ function Questionnaire() {
 			},
 		
 	]
-    const [answers, setAnswer] = useState("");
+	const [questionnaire, setQuestions] = useState(questions)
+    const [answers, setAnswer] = useState(" ");
 	const [currentQuestion, setCurrentQuestion] = useState(0);
 	const [initialAnswer, setInitial] = useState([])
+	const [element, setElement] = useState("")
+
+	// Radio element
+const radio = <div className='answer-section' onChange={(e) => {setInitialAnswer(e)}}>
+				{questionnaire[currentQuestion]?.answerOptions.map((answerOption, i) => (
+						<FormCheck key={i} 
+						type={"checkbox"}
+						id={"default-checkbox"}
+						label={answerOption?.answerText}
+						value={answerOption?.index}>
+					</FormCheck>
+				))}
+				</div>
+
+let choice;
+let input = <div className='answer-section'>
+				<input></input>
+			</div>
+
+	useEffect(() => {
+		setQuestions(questions)
+		switch (questionnaire[currentQuestion].type) {
+			case "radio":
+				setElement(radio)
+				break;
+			case "choice":
+				setElement(choice)
+				break;		
+			case "input":
+				setElement(input)
+				break;
+			default:
+				break;
+		}
+	}, []);
 
     const handleAnswer = (data) => {
-        const nextQuestion = currentQuestion + 1;
-        if (nextQuestion < questions.length) {
+		let array = questionnaire;
+		if (initialAnswer.includes("property")) {
+			for (let i = 0; i < property.length; i++) {
+				array.push(property[i])
+			}
+		setQuestions(array);
+		}
+		const nextQuestion = currentQuestion + 1;
+        if (nextQuestion < questionnaire.length) {
 			setCurrentQuestion(nextQuestion);
 		} 
         setAnswer(data);
+		switch (questionnaire[currentQuestion].type) {
+			case "radio":
+				setElement(radio)
+				break;
+			case "choice":
+				setElement(choice)
+				break;		
+			case "input":
+				setElement(input)
+				break;
+			default:
+				break;
+		}
+		
     }
 
 	const setInitialAnswer= (event) => {
@@ -92,12 +154,9 @@ function Questionnaire() {
 			result = arrayText.replace(text,"")
 			}
 		setInitial(arr)
-		console.log(event.target.nextElementSibling)
 		setAnswer(result)
-			}
-	console.log(initialAnswer)
-	console.log(answers)
-
+	}
+	console.log(questionnaire)
         return (
             <div className='container-question'>
                 <h1>Welcome</h1>
@@ -105,21 +164,12 @@ function Questionnaire() {
                     <div className='app'>
                             <div className='question-section'>
                                 <div className='question-count'>
-                                    <span>Question {currentQuestion + 1}</span>/{questions.length}
+                                    <span>Question {currentQuestion + 1}</span>/{questionnaire?.length}
                                 </div>
-                                <div className='question-text'>{questions[currentQuestion].questionText}</div>
+                                <div className='question-text'>{questionnaire[currentQuestion]?.questionText}</div>
                             </div>
 							<Form>
-                            <div className='answer-section' onChange={(e) => {setInitialAnswer(e)}}>
-                                {questions[currentQuestion].answerOptions.map((answerOption, i) => (
-										<FormCheck key={i} 
-										type={"checkbox"}
-										id={"default-checkbox"}
-										label={answerOption.answerText}
-										value={answerOption.index}>
-									</FormCheck>
-                                ))}
-                            </div>
+							{element}
 							<Button onClick={() => {handleAnswer()}}> Submit </Button>
 							</Form>
                 </div>
