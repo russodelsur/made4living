c93c6cd574ce1c6919f586078b8a0227b35669aa
Ruslan <ruslan@m2measure.co.uk>
2023-05-28 14:36:12 +0100

    Improved XML sitemap

--- src/components/pages/Work.js
+++ src/components/pages/Work.js
@@ -1,23 +1,23 @@
 import React from 'react';
 import { Container, Card, Col, Row } from 'react-bootstrap';
-import { useNavigate } from 'react-router-dom';
+import {Link} from 'react-router-dom';
 import projects from "../../data.json"
 
 function Work() {
-
-    const navigate = useNavigate();
-        return (
+    return (
             <Container>                
                 <Row xs={1} md={2} className="g-4">
                     {projects.projects.map((project, index) => (
                     <Col key={index}>
-                        <Card onClick={()=>{navigate(`/projects/${project.slug}`)}} className='card-projects' bg='light'>
+                        <Link to={`/projects/${project.slug}`} id={project.name}>
+                        <Card className='card-projects' bg='light'>
                             <Card.Img  variant="top" className='service-image'src={require("../../img/"+project.image+".jpg")} />
                             <Card.Body>
                                 <Card.Title>{project.name}</Card.Title>
                                 <Card.Subtitle className="mb-2 text-muted">{project.type}</Card.Subtitle>
                             </Card.Body>
                         </Card>
+                        </Link>
                     </Col>
                     ))}
                 </Row>
