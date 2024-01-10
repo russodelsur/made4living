d781bfd5482f672091403cb8952145efebacfbd7
russodelsur <ruslan@m2measure.co.uk>
2024-01-10 19:18:39 +0000

    Update build directory and remove react-snap

--- src/index.js
+++ src/index.js
@@ -4,16 +4,16 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createRoot } from 'react-dom/client';
-import { hydrateRoot } from 'react-dom/client';
+// import { hydrateRoot } from 'react-dom/client';
 
 const rootElement = document.getElementById("root");
 const root = createRoot(rootElement);
-if (rootElement.hasChildNodes()) {
-  hydrateRoot(rootElement, <App />)
-} else {
-  root.render(<App />);
-}
-// root.render(<App />, rootElement);
+// if (rootElement.hasChildNodes()) {
+//   hydrateRoot(rootElement, <App />)
+// } else {
+//   root.render(<App />);
+// }
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
